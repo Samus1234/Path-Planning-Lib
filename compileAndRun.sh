192d649497bc195080f422842969ebe2ebe842c2
Siddharth Gangadhar <sgangadhar@constructionrobots.com>
2024-12-02 13:43:52 -0500

    Added build folder

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,3 +1,5 @@
-clang++ -Wall -O2 -std=c++20 -I/usr/include/python3.10 -lpython3.10 -o $1 $1.cpp
+mkdir -p build
 
-./$1
+clang++ -Wall -O2 -std=c++20 -I/usr/include/python3.10 -lpython3.10 -o build/$1 $1.cpp
+
+./build/$1
