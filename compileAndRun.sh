fdf69c9db18f6232a443abe9968abe0e4e81f063
Siddharth Gangadhar <sgangadhar@constructionrobots.com>
2024-12-02 13:50:24 -0500

    Improved compile and build functionality

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,5 +1,9 @@
+filename=$1
+
+executable_name="${filename%.*}"
+
 mkdir -p build
 
-clang++ -Wall -O2 -std=c++20 -I/usr/include/python3.10 -lpython3.10 -o build/$1 $1.cpp
+clang++ -Wall -O2 -std=c++20 -I/usr/include/python3.10 -lpython3.10 -o build/${executable_name} ${filename}
 
-./build/$1
+./build/${executable_name}
