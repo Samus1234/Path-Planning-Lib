a2c8db55e876d5bbfd68903ee56228a6e0273baa
Samus1234 <sidd.gangadhar@gmail.com>
2024-12-02 13:30:59 -0500

    Update path_planning_utils.hpp
    Fixed a blunder

--- path_planning_utils.hpp
+++ path_planning_utils.hpp
@@ -1,184 +1,218 @@
-#include "path_planning_utils.hpp"
-#include <limits>
+#ifndef _PLANNING_UTILS_H_
+#define _PLANNING_UTILS_H_
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <cmath>
 #include <memory>
+#include "graph.hpp"
 
-template<typename Graph>
-class Astar {
-    using ValueType = typename Graph::ValueType;
-    using NodeType = typename Graph::NodeType;
-    using NodePtr = typename Graph::NodePtr;
-    using Iterator = typename Graph::Iterator;
-    using CostType = float;
-    using NodeQueueType = typename std::pair<CostType, NodePtr>;
-    static constexpr CostType INF = std::numeric_limits<CostType>::max();
+namespace PathPlanningUtils {
+
+struct Cell {
+    int x, y;
+    Cell(int x, int y) : x(x), y(y) {}
+
+    bool operator==(const Cell& other) const {
+        return x == other.x && y == other.y;
+    }
+};
+
+struct CellHash {
+    std::size_t operator()(const Cell& cell) const {
+        return std::hash<int>()(cell.x) ^ (std::hash<int>()(cell.y) << 1);
+    }
+};
+
+using CellGraph = Graph<Cell, unsigned, true, true, CellHash>;
+
+class OccupancyGrid {
 public:
-    Astar() = default;
-    ~Astar() = default;
-
-    Astar(std::shared_ptr<Graph> graph) { setGraph(graph); }
-
-    void setGraph(std::shared_ptr<Graph> graph) {
-        graph_ = graph;
-        initialize();
+    OccupancyGrid(size_t width, size_t height) : width_(width), height_(height) {
+        grid_.resize(height, std::vector<int>(width, 0));
     }
 
-    CostType heuristicCost(const ValueType& cell_a, const ValueType& cell_b) {
-        const char TYPE = 'C';
-        CostType dx = cell_b.x - cell_a.x;
-        CostType dy = cell_b.y - cell_a.y;
-        switch (TYPE) {
-        case 'E':
-            return std::sqrt(dx*dx + dy*dy);
-            break;
+    size_t width() const {
+        return width_;
+    }
 
-        case 'M':
-            return std::abs(dx) + std::abs(dy);
-            break;
+    size_t height() const {
+        return height_;
+    }
 
-        case 'C':
-            return std::max(std::abs(dx), std::abs(dy));
-            break;
-        
-        default:
-            throw std::runtime_error("Invalid Measurement norm!");
-            break;
+    void addObstacle(int x, int y) {
+        if (isValid(x, y)) {
+            grid_[y][x] = 1;
         }
     }
 
-    void initialize() {
-        for (auto it = graph_->begin(); it != graph_->end(); ++it) {
-            g_cost_[it.getNode()] = INF;
-            h_cost_[it.getNode()] = INF;
-            f_cost_[it.getNode()] = INF;
+    void removeObstacle(int x, int y) {
+        if (isObstacle(x, y)) {
+            grid_[y][x] = 0;
         }
     }
 
-    void setStartAndGoal(const ValueType& start, const ValueType& goal) {
-        start_node_ = graph_->getNode(start);
-        goal_node_ = graph_->getNode(goal);
-        
-        if (start_node_ == nullptr || goal_node_ == nullptr) {
-            throw std::runtime_error("Invalid start and goal nodes!");
+    void setPath(int x, int y) {
+        if(isFree(x, y)) {
+            grid_[y][x] = -1;
         }
-        g_cost_[start_node_] = 0;
-        node_priority_queue_.emplace(0, start_node_);
     }
 
-    void runOnce() {
-        if (node_priority_queue_.empty()) {
-            complete_ = true;
-            return;
-        }
+    void setStart(int x, int y) {
+        start_ = Cell(x, y);
+    }
 
-        auto [current_cost, current_node] = node_priority_queue_.top();
-        node_priority_queue_.pop();
+    void setGoal(int x, int y) {
+        goal_ = Cell(x, y);
+    }
 
-        if (current_node == goal_node_) {
-            complete_ = true;
-            return;
-        }
+    bool isFree(int x, int y) const {
+        return isValid(x, y) && grid_[y][x] == 0;
+    }
 
-        for (const auto& [neighbor, tentative_g_cost] : current_node->neighbors_) {
-            auto heuristic_cost = heuristicCost(neighbor->data_, goal_node_->data_);
-            CostType cost_to_go = (CostType) tentative_g_cost + current_cost + heuristic_cost;
-            if (cost_to_go < f_cost_[neighbor]) {
-                g_cost_[neighbor] = (CostType) tentative_g_cost;
-                h_cost_[neighbor] = heuristic_cost;
-                f_cost_[neighbor] = cost_to_go;
-                node_priority_queue_.emplace(cost_to_go, neighbor);
-                predecessor_map_[neighbor] = current_node;
+    bool isObstacle(int x, int y) const {
+        return isValid(x, y) && grid_[y][x] == 1;
+    }
+
+    bool isValid(int x, int y) const {
+        return x >= 0 && y >= 0 && x < width_ && y < height_;
+    }
+
+    void display() const {
+        for (int y = 0; y < height_; ++y) {
+            for (int x = 0; x < width_; ++x) {
+                if (x == start_.x && y == start_.y) {
+                    std::cout << "S ";
+                } else if (x == goal_.x && y == goal_.y) {
+                    std::cout << "G ";
+                } else if (grid_[y][x] == -1) {
+                    std::cout << "\u2394 ";
+                } else if (grid_[y][x] == 1) {
+                    std::cout << "\u25A0 ";
+                } else {
+                    std::cout << ". ";
+                }
             }
+            std::cout << "\n";
         }
     }
 
-    void run() {
-        while (!complete_) {
-            runOnce();
-        }
+    const Cell& getStart() const {
+        return start_;
     }
 
-    void printCostToGo() {
-        for (const auto& [node, cost] : f_cost_) {
-            if (cost < INF) {
-                std::cout << "Cost to go from " <<
-                    start_node_->data_ << " to " <<
-                    node->data_ << " is " << cost << "\n";
-            } else {
-                std::cout << "Cost to go from " <<
-                    start_node_->data_ << " to " <<
-                    node->data_ << " is " << "infinity" << "\n";
-            }
-        }
-    }
-
-    std::stack<ValueType> findPath() {
-        if (!complete_) {
-            run();
-        }
-        
-        auto node = goal_node_;
-        if (f_cost_[node] == INF) {
-            throw std::runtime_error("Path does not exist!");
-        }
-
-        std::stack<ValueType> path_stack;
-        
-        while (node != start_node_) {
-            path_stack.emplace(node->data_);
-            node = predecessor_map_[node];
-        }
-        path_stack.emplace(node->data_);
-        return path_stack;
-    }
-
-    void printPath() {
-
-        auto path_stack = findPath();
-
-        while (!path_stack.empty()) {
-            auto path_node = path_stack.top();
-            path_stack.pop();
-            std::cout << path_node->data_ << " -> ";
-        }
-        std::cout << "END" << std::endl;
+    const Cell& getGoal() const {
+        return goal_;
     }
 
 private:
-    bool complete_{false};
-    NodePtr start_node_{nullptr};
-    NodePtr goal_node_{nullptr};
-    std::shared_ptr<Graph> graph_;
-    std::unordered_map<NodePtr, CostType> g_cost_;
-    std::unordered_map<NodePtr, CostType> h_cost_;
-    std::unordered_map<NodePtr, CostType> f_cost_;
-    std::unordered_map<NodePtr, NodePtr> predecessor_map_; // Child -> Parent
-    std::priority_queue<NodeQueueType, std::vector<NodeQueueType>, std::greater<void>> node_priority_queue_;
+    size_t width_, height_;
+    std::vector<std::vector<int>> grid_;
+    Cell start_{0, 0};
+    Cell goal_{0, 0};
 };
 
-int main(int argc, char** argv) {
+void createGraphFromGrid(const OccupancyGrid& grid, std::shared_ptr<CellGraph> graph) {
+    // Add all grid cells to graph
+    for (int x = 0; x < grid.width(); ++x) {
+        for (int y = 0; y < grid.height(); ++y) {
+            if (grid.isFree(x, y)) {
+                graph->insertNode(Cell(x, y));
+            }
+        }
+    }
+    // Next, add all the edges, i.e., neioghbors
+    for (int x = 0; x < grid.width(); ++x) {
+        for (int y = 0; y < grid.height(); ++y) {
+            if (grid.isFree(x, y)) {
+                Cell current_cell(x, y);
+                // Vector of primitive motion directions
+                const std::vector<std::pair<int, int>> directions = {
+                    {0, 1},  {0, -1},
+                    {1, 0},  {-1, 0},
+                    {1, 1},  {-1, 1},
+                    {1, -1},  {-1, -1},
+                };
+                for (const auto& [dx, dy] : directions) {
+                    int nx = x + dx;
+                    int ny = y + dy;
+                    if (grid.isValid(nx, ny) && !grid.isObstacle(nx, ny)) {
+                        Cell neighbor_cell(nx, ny);
+                        if (dx*dy == 0) {
+                            graph->addEdge(current_cell, neighbor_cell, 1000);
+                        } else {
+                            graph->addEdge(current_cell, neighbor_cell, 1414);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
-    auto graph = std::make_shared<PathPlanningUtils::CellGraph>();
+OccupancyGrid createTestGrid() {
+    OccupancyGrid grid(80, 80);
 
-    auto grid = PathPlanningUtils::createTestGrid();
-
-    PathPlanningUtils::createGraphFromGrid(grid, graph);
-
-    std::cout << "Number of Vertices: " << graph->numVertices() << " and number of edges: " << graph->numEdges() << std::endl;
-
-    Astar<PathPlanningUtils::CellGraph> planner(graph);
-
-    planner.setStartAndGoal(grid.getStart(), grid.getGoal());
-
-    auto path_stack = planner.findPath();
-
-    while (!path_stack.empty()) {
-        auto path_cell = path_stack.top();
-        path_stack.pop();
-        grid.setPath(path_cell.x, path_cell.y);
+    // Define outer boundaries (walls)
+    for (int x = 0; x < 80; ++x) {
+        grid.addObstacle(x, 0);      // Top boundary
+        grid.addObstacle(x, 79);     // Bottom boundary
+    }
+    for (int y = 0; y < 80; ++y) {
+        grid.addObstacle(0, y);      // Left boundary
+        grid.addObstacle(79, y);     // Right boundary
     }
 
-    std::cout << "Occupancy Grid:\n";
-    grid.display();
-    
-    return 0;
+    // Horizontal and vertical walls creating rooms
+    for (int x = 10; x < 70; ++x) {
+        grid.addObstacle(x, 20);     // Horizontal wall 1
+        grid.addObstacle(x, 40);     // Horizontal wall 2
+        grid.addObstacle(x, 60);     // Horizontal wall 3
+    }
+    for (int y = 10; y < 70; ++y) {
+        grid.addObstacle(20, y);     // Vertical wall 1
+        grid.addObstacle(40, y);     // Vertical wall 2
+        grid.addObstacle(60, y);     // Vertical wall 3
+    }
+
+    // Doors (gaps in walls for corridors)
+    for (int x = 25; x <= 35; x += 10) {
+        grid.removeObstacle(x, 20);  // Doors in horizontal wall 1
+        grid.removeObstacle(x, 40);  // Doors in horizontal wall 2
+        grid.removeObstacle(x, 60);  // Doors in horizontal wall 3
+    }
+    for (int y = 25; y <= 65; y += 10) {
+        grid.removeObstacle(20, y);  // Doors in vertical wall 1
+        grid.removeObstacle(40, y);  // Doors in vertical wall 2
+        grid.removeObstacle(60, y);  // Doors in vertical wall 3
+    }
+
+    // Additional obstacles for complexity
+    for (int x = 5; x <= 15; ++x) {
+        for (int y = 5; y <= 15; ++y) {
+            grid.addObstacle(x, y);  // Top-left room obstacles
+        }
+    }
+    for (int x = 65; x <= 75; ++x) {
+        for (int y = 65; y <= 75; ++y) {
+            grid.addObstacle(x, y);  // Bottom-right room obstacles
+        }
+    }
+    for (int x = 45; x <= 55; ++x) {
+        for (int y = 45; y <= 55; ++y) {
+            grid.addObstacle(x, y);  // Central room obstacles
+        }
+    }
+
+    // Setting start and goal points
+    grid.setStart(1, 1);             // Start near the top-left corner
+    grid.setGoal(78, 78);            // Goal near the bottom-right corner
+
+    return grid;
 }
+
+} // end PathPlanningUtils
+
+#endif /* _PLANNING_UTILS_H_ */
